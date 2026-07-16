092dd74fbbe4b6a5a9bf48c9fc1c9cd5de591a5e
MEFmonster <166375888+MEFmonster@users.noreply.github.com>
2026-07-17 03:23:27 +0700

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const audio = document.createElement('audio');
+    audio.src = '/music/YA_dyshu-Irina_Allegrova_-_Den_Rozhdeniya_-_S_Dnjom_Rozhdeniya_Uspekha_radosti_vezeniya_lyubykh_zhelanijj_ispolneniya_i_million_nochejj_i_dnejj_S_Dnjom_Rozhdeniya_Lyubvi_do_golovokruzheniya_i_74820012.mp3';
+    audio.play(); // Запускаем воспроизведение
+});
